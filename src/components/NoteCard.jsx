f4b71ea63da8d1eaa7d2d1dfdcc6cbb77d42c60e
Abinash Karan <86921957+abinash2553J@users.noreply.github.com>
2026-09-07 11:32:11 +0530

    added the drag and drop feature with autogrow text area

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,19 +1,70 @@
-import React from 'react'
+import { useRef, useEffect, useState } from 'react'
+import Trash from '../icons/Trash'
+import { setNewOffset, autoGrow, setZIndex } from '../utils.js'
 
 const NoteCard = ({ note }) => {
     const body = JSON.parse(note.body);
-    const position = JSON.parse(note.position);
+    const [position, setPositon] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
+    const textAreaRef = useRef(null);
+
+    let mouseStartPos = { x: 0, y: 0 };
+    const cardRef = useRef(null);
+
+    useEffect(() => {
+        autoGrow(textAreaRef);
+    }, []);
+
+    const mouseDown = (e) => {
+        setZIndex(cardRef.current);
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    };
+
+    const mouseMove = (e) => {
+        //1 - Calculate move direction
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+
+        //2 - Update start position for next move.
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+
+        //3 - Update card top and left position.
+        setPositon(newPosition);
+    };
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    };
+
+
     return (
-        <div className="card" style={{ backgroundColor: colors.colorBody, }}>
-            <div className="card-header" style = {{ backgroundColor: colors.colorHeader }}></div>
+        <div className="card" ref={cardRef} style={{ backgroundColor: colors.colorBody, left: `${position.x}px`, top: `${position.y}px`, }}>
+            <div className="card-header" style={{ backgroundColor: colors.colorHeader }} onMouseDown={mouseDown}>
+                <Trash />
+            </div>
             <div className="card-body">
                 <textarea
+                    ref={textAreaRef}
                     style={{ color: colors.colorText }}
                     defaultValue={body}
+                    onInput={() => {
+                        autoGrow(textAreaRef);
+                    }}
+                    onFocus={() => {
+                        setZIndex(cardRef.current);
                 ></textarea>
-            </div>
         </div>
+        </div >
     )
 }
 
